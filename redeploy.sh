2680df344f06cf9182f869ca4d00d9d5bf1d4256
Wojciech Grajewski <wojtasskorcz@gmail.com>
2013-09-10 15:33:36 +0000

    Fixed pom.xml to download snapshots. Reverted redeploy.sh to undeploy the app

--- redeploy.sh
+++ redeploy.sh
@@ -4,4 +4,5 @@ mvn clean install -DskipTests
 cp src/test/java/test.properties target/test-classes/
 cp target/ids-1.0.0.war install/
 cd install/
+./ids_setup.py --undeploy
 ./ids_setup.py --deploy
