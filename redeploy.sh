4fc9d97840b5683d35e3c8b3f89f8e85e6ffbee5
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-09-10 14:50:15 +0000

    IDS 0.0 to ids 1.0.0

--- redeploy.sh
+++ redeploy.sh
@@ -2,6 +2,6 @@
 
 mvn clean install -DskipTests
 cp src/test/java/test.properties target/test-classes/
-cp target/IDS-0.0.war install/
+cp target/ids-1.0.0.war install/
 cd install/
 ./ids_setup.py --deploy
