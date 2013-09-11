1119ccaa2991309455797eff0b5f5eb6f0c2aa65
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-09-11 15:56:30 +0000

    Moved test.properties again

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 mvn clean install -DskipTests
-cp src/test/java/test.properties target/test-classes/
 asadmin undeploy ids-1.0.0-SNAPSHOT
 asadmin deploy --contextroot ids target/ids-1.0.0-SNAPSHOT.war | grep -v PER0
