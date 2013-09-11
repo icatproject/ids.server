583b3d1aa3a6ad51bf38f010eb94b98b931c7899
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-09-11 15:35:50 +0000

    Installation changes

--- redeploy.sh
+++ redeploy.sh
@@ -3,4 +3,4 @@
 mvn clean install -DskipTests
 cp src/test/java/test.properties target/test-classes/
 asadmin undeploy ids-1.0.0-SNAPSHOT
-asadmin deploy target/ids-1.0.0-SNAPSHOT.war | grep -v PER0
+asadmin deploy --contextroot ids target/ids-1.0.0-SNAPSHOT.war | grep -v PER0
