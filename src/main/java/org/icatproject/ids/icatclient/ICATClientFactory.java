9834ba1f240299660291878e423056ab4b26bc1d
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-09-12 08:56:43 +0000

    Support 4.3 too

--- src/main/java/org/icatproject/ids/icatclient/ICATClientFactory.java
+++ src/main/java/org/icatproject/ids/icatclient/ICATClientFactory.java
@@ -7,35 +7,34 @@ import org.icatproject.ids.util.PropertyHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
 public class ICATClientFactory {
 
-    private final static Logger logger = LoggerFactory.getLogger(ICATClientFactory.class);
+	private final static Logger logger = LoggerFactory.getLogger(ICATClientFactory.class);
 
-    private static ICATClientFactory instance = new ICATClientFactory();
-    private static String url;
+	private static ICATClientFactory instance = new ICATClientFactory();
+	private static String url;
 
-    private ICATClientFactory() {
-        url = PropertyHandler.getInstance().getIcatUrl();
-    }
+	private ICATClientFactory() {
+		url = PropertyHandler.getInstance().getIcatUrl();
+	}
 
-    public static ICATClientFactory getInstance() {
-        return instance;
-    }
+	public static ICATClientFactory getInstance() {
+		return instance;
+	}
 
-    public ICATClientBase createICATInterface() throws MalformedURLException, ICATClientException {
-        ICATClientBase icat = null;
+	public ICATClientBase createICATInterface() throws MalformedURLException, ICATClientException {
+		ICATClientBase icat = null;
 
-        // Using client for ICAT 4.2 get major and minor version number of requested ICAT
-        ICATClient42 tempICATClient = new ICATClient42(url);
-        String version = tempICATClient.getICATVersion().substring(0, 3);
+		// Using client for ICAT 4.2 get major and minor version number of requested ICAT
+		ICATClient42 tempICATClient = new ICATClient42(url);
+		String version = tempICATClient.getICATVersion().substring(0, 3);
 
-        if (version.equals("4.2")) {
-            icat = new ICATClient42(url);
-        } else {
-            logger.error("Unsupported ICAT version : '" + tempICATClient.getICATVersion() + "'");
-        }
-        
-        return icat;
-    }
+		if (version.equals("4.2") || version.equals("4.3")) {
+			icat = new ICATClient42(url);
+		} else {
+			logger.error("Unsupported ICAT version : '" + tempICATClient.getICATVersion() + "'");
+		}
+
+		return icat;
+	}
 }
