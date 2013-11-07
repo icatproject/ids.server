dd5d8ba8419ba3abb72f38fbebcfa2bf3a81d5ee
Steve Fisher <dr.s.m.fisher@gmail.com>
2013-11-07 13:42:35 +0000

    Reduce levels of directory

--- src/main/java/org/icatproject/ids/webservice/Status.java
+++
@@ -1,9 +0,0 @@
-package org.icatproject.ids.webservice;
-
-/*
- *  Theses are the values that the getStatus methods can return. 
- *  They are only used when returning status to the client, nowhere else.
- */
-public enum Status {
-	ONLINE, RESTORING, ARCHIVED
-}
