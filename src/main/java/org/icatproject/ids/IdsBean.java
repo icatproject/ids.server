0015acbca6e2269937c48b83c79466c74da99bc9
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2018-01-19 15:09:32 +0100

    Handle case location null for getSize().

--- src/main/java/org/icatproject/ids/IdsBean.java
+++ src/main/java/org/icatproject/ids/IdsBean.java
@@ -1176,7 +1176,10 @@ public class IdsBean {
 			if (size == 0) {
 				try {
 					if (dfids.size() != 0) {
-						icat.get(sessionId, "Datafile", dfids.get(0));
+						Datafile datafile = (Datafile) icat.get(sessionId, "Datafile", dfids.get(0));
+						if (datafile.getLocation() == null) {
+							throw new NotFoundException("Datafile not found");
+						}
 					}
 					if (dsids.size() != 0) {
 						icat.get(sessionId, "Dataset", dsids.get(0));
@@ -1255,7 +1258,7 @@ public class IdsBean {
 	}
 
 	private long getSizeFor(String sessionId, StringBuilder sb) throws InternalException {
-		String query = "SELECT SUM(df.fileSize) from Datafile df WHERE df.id IN (" + sb.toString() + ")";
+		String query = "SELECT SUM(df.fileSize) from Datafile df WHERE df.id IN (" + sb.toString() + ") AND df.location IS NOT NULL";
 		try {
 			return (Long) icat.search(sessionId, query).get(0);
 		} catch (IcatException_Exception e) {
@@ -1266,7 +1269,7 @@ public class IdsBean {
 	}
 
 	private long evalSizeFor(String sessionId, String where, StringBuilder sb) throws InternalException {
-		String query = "SELECT SUM(df.fileSize) from Datafile df WHERE " + where + " IN (" + sb.toString() + ")";
+		String query = "SELECT SUM(df.fileSize) from Datafile df WHERE " + where + " IN (" + sb.toString() + ") AND df.location IS NOT NULL";
 		logger.debug("icat query for size: {}", query);
 		try {
 			return (Long) icat.search(sessionId, query).get(0);
