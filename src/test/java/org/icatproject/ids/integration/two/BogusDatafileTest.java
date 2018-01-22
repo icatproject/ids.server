819aec4ff628482258f5abde13344fc5c734a0e1
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2018-01-22 17:03:26 +0100

    Add a test for the full put, write, archive, and restore cycle.

--- src/test/java/org/icatproject/ids/integration/two/BogusDatafileTest.java
+++ src/test/java/org/icatproject/ids/integration/two/BogusDatafileTest.java
@@ -1,9 +1,16 @@
 package org.icatproject.ids.integration.two;
 
+import java.io.File;
 import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -30,6 +37,8 @@ import org.icatproject.ids.integration.util.client.TestingClient.Status;
 
 public class BogusDatafileTest extends BaseTest {
 
+	private static long timestamp = System.currentTimeMillis();
+
 	@BeforeClass
 	public static void setup() throws Exception {
 		setup = new Setup("two.properties");
@@ -38,7 +47,6 @@ public class BogusDatafileTest extends BaseTest {
 
 	@Before
 	public void createBogusFiles() throws Exception {
-		long timestamp = System.currentTimeMillis();
 
 		Dataset ds1 = (Dataset) icatWS.get(sessionId, "Dataset", datasetIds.get(0));
 		Datafile dfb1 = new Datafile();
@@ -64,6 +72,7 @@ public class BogusDatafileTest extends BaseTest {
 		datafileIds.add(dfb1.getId());
 		datafileIds.add(dfb2.getId());
 		datafileIds.add(dfb3.getId());
+
 	}
 
 	@Test
@@ -130,6 +139,49 @@ public class BogusDatafileTest extends BaseTest {
 
 		DataSelection selection = new DataSelection().addDatafile(datafileIds.get(5));
 		testingClient.getSize(sessionId, selection, 404);
+
+	}
+
+	/*
+	 * Try the full cycle: upload a new file into a dataset having a bogus file, which triggers a write of the
+	 * dataset to archive storage, archive the dataset, and restore it.  Each step must deal gracefully with
+	 * the bogus file in the dataset.
+	 */
+	@Test
+	public void putWriteArchiveRestore() throws Exception {
+
+		Long dsId = datasetIds.get(0);
+		DataSelection selection = new DataSelection().addDataset(dsId);
+		Path dirOnFastStorage = getDirOnFastStorage(dsId);
+
+		testingClient.restore(sessionId, selection, 204);
+		waitForIds();
+		assertTrue(Files.exists(dirOnFastStorage));
+
+		Long dfulId = testingClient.put(sessionId, Files.newInputStream(newFileLocation),
+				"uploaded_file_" + timestamp, dsId, supportedDatafileFormat.getId(),
+				"A rather splendid datafile", 201);
+		Datafile dful = (Datafile) icatWS.get(sessionId, "Datafile", dfulId);
+		testingClient.archive(sessionId, selection, 204);
+		waitForIds();
+		assertFalse(Files.exists(dirOnFastStorage));
+
+		testingClient.restore(sessionId, selection, 204);
+		waitForIds();
+		assertTrue(Files.exists(dirOnFastStorage));
+
+		File[] filesList = dirOnFastStorage.toFile().listFiles();
+		assertEquals(3, filesList.length);
+		Set<String> locations = new HashSet<>();
+		Datafile df1 = (Datafile) icatWS.get(sessionId, "Datafile", datafileIds.get(0));
+		locations.add(getLocationFromDigest(df1.getId(), df1.getLocation()));
+		Datafile df2 = (Datafile) icatWS.get(sessionId, "Datafile", datafileIds.get(1));
+		locations.add(getLocationFromDigest(df2.getId(), df2.getLocation()));
+		locations.add(getLocationFromDigest(dful.getId(), dful.getLocation()));
+		for (File file : filesList) {
+			String location = setup.getStorageDir().relativize(file.toPath()).toString();
+			assertTrue(locations.contains(location));
+		}
 
 	}
 
