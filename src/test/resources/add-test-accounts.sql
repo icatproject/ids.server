e01572713151d2f3b7db3615787d472d4140adb6
Rolf Krahl <rolf.krahl@helmholtz-berlin.de>
2021-04-06 16:06:37 +0200

    Add a SQL script to create the accounts needed in the tests

---
+++ src/test/resources/add-test-accounts.sql
@@ -0,0 +1,12 @@
+--
+-- Add the test users accounts needed by ids.server tests.
+--
+--
+
+INSERT INTO PASSWD VALUES ('notroot','password');
+INSERT INTO PASSWD VALUES ('piOne','piOne');
+INSERT INTO PASSWD VALUES ('piTwo','piTwo');
+INSERT INTO PASSWD VALUES ('root','password');
+INSERT INTO PASSWD VALUES ('guest','guess');
+INSERT INTO PASSWD VALUES ('CIC','password');
+INSERT INTO PASSWD VALUES ('reader','readerpw');
