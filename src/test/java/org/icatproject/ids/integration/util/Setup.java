7d859d5df9d5a1691f134e02fc1fcc3c27fcf938
kevinphippsstfc <kevinphippsstfc@users.noreply.github.com>
2021-06-29 12:30:35 +0100

    Refactor and remove old unused bits

--- src/test/java/org/icatproject/ids/integration/util/Setup.java
+++ src/test/java/org/icatproject/ids/integration/util/Setup.java
@@ -1,78 +1,56 @@
 package org.icatproject.ids.integration.util;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.icatproject.ICAT;
-import org.icatproject.ids.Constants;
 import org.icatproject.ids.ICATGetter;
 import org.icatproject.ids.TestUtils;
 import org.icatproject.utils.CheckedProperties;
 import org.icatproject.utils.ShellCommand;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
- * Setup the test environment for the IDS. This is done by reading property
- * values from the test.properties file and calling this class before 
- * running the tests.
+ * Setup the test environment for the IDS. 
+ * 
+ * This is done by reading property values from test.properties and 
+ * run.properties and calling a script to (re)install the IDS. 
+ * 
+ * IcatSetup is then called to ensure that the ICAT being used is populated 
+ * with the necessary metadata.
  */
 public class Setup {
 
-	private URL icatUrl = null;
-	private URL idsUrl = null;
+	private static final Logger logger = LoggerFactory.getLogger(Setup.class);
 
-	private String rootSessionId = null;
-	private String forbiddenSessionId = null;
+	private static boolean skipIcatRepopulate = false;
+
+	private URL icatUrl;
+	private URL idsUrl;
+
+	private String rootSessionId;
+	private String forbiddenSessionId;
+
+	private ICAT icatSoapClient;
 
 	private Path home;
-	private Path storageArchiveDir;
-	private Path storageDir;
-	private Path preparedCacheDir;
-	private Path updownDir;
+	private Path pluginMainDir;
 
-	public boolean isTwoLevel() {
-		return twoLevel;
-	}
-
-	public String getStorageUnit() {
-		return storageUnit;
-	}
-
-	public Path getErrorLog() {
-		return errorLog;
-	}
-
-	private Path errorLog;
-	private String storageUnit;
-	private boolean twoLevel;
-	private String key;
-
-	// TODO: probably remove this constructor
-	// added for testing to skip redeploy of IDS on each test run
-	public Setup() throws Exception {
-		// Start by reading the test properties
-		Properties testProps = new Properties();
-		InputStream is = Setup.class.getClassLoader().getResourceAsStream("test.properties");
-		try {
-			testProps.load(is);
-		} catch (Exception e) {
-			System.err.println("Problem loading test.properties: " + e.getClass() + " " + e.getMessage());
-		}
-
-		String serverUrl = System.getProperty("serverUrl");
-		icatUrl = new URL(serverUrl);
-		ICAT icat = ICATGetter.getService(serverUrl);
-		rootSessionId = TestUtils.login(icat, testProps.getProperty("login.root"));
-		idsUrl = new URL(serverUrl + "/ids");
-	}
 
 	public Setup(String runPropertyFile) throws Exception {
+		doSetup(runPropertyFile);
+	}
 
+	public Setup(String runPropertyFile, boolean skipIcatRepopulate) throws Exception {
+		Setup.skipIcatRepopulate = skipIcatRepopulate;
+		doSetup(runPropertyFile);
+	}
+
+	private void doSetup(String runPropertyFile) throws Exception {
 		// Test home directory
 		String testHome = System.getProperty("testHome");
 		if (testHome == null) {
@@ -89,8 +67,6 @@ public class Setup {
 			System.err.println("Problem loading test.properties: " + e.getClass() + " " + e.getMessage());
 		}
 
-		setReliability(1.);
-
 		String serverUrl = System.getProperty("serverUrl");
 
 		idsUrl = new URL(serverUrl + "/ids");
@@ -106,89 +82,69 @@ public class Setup {
 		ShellCommand sc = new ShellCommand("src/test/scripts/" + prepareScript, 
 				"src/test/resources/" + runPropertyFile,
 				home.toString(), containerHome, serverUrl);
-		System.out.println(prepareScript + " exit code is: " + sc.getExitValue());
-		System.out.println(prepareScript + " std out is: " + sc.getStdout());
-		System.out.println(prepareScript + " std err is: " + sc.getStderr());
+		logger.debug("{} exit code is: {}", prepareScript, sc.getExitValue());
+		logger.debug("{} std out is: {}", prepareScript, sc.getStdout());
+		logger.debug("{} std err is: {}", prepareScript, sc.getStderr());
 		if (sc.getExitValue() != 0) {
 			throw new Exception(prepareScript + " failed with message: " + sc.getMessage());
 		}
-		System.out.println(
-				"Setting up " + runPropertyFile + " took " + (System.currentTimeMillis() - time) / 1000. + "seconds");
+		logger.info("Setting up {} took {} seconds", runPropertyFile, (System.currentTimeMillis() - time) / 1000.);
 
-		// Having set up the ids.properties file read it find other things
+		// Having set up the run.properties file read it find other things
 		CheckedProperties runProperties = new CheckedProperties();
 		runProperties.loadFromFile("src/test/install/run.properties");
-		if (runProperties.has("key")) {
-			key = runProperties.getString("key");
-		}
-		updownDir = home.resolve(testProps.getProperty("updownDir"));
 		icatUrl = runProperties.getURL("icat.url");
 		ICAT icat = ICATGetter.getService(icatUrl.toString());
 		rootSessionId = TestUtils.login(icat, testProps.getProperty("login.root"));
 		forbiddenSessionId = TestUtils.login(icat, testProps.getProperty("login.unauthorized"));
 
-		storageDir = runProperties.getPath("plugin.main.dir");
-
-		if (runProperties.has("plugin.archive.dir")) {
-			storageArchiveDir = runProperties.getPath("plugin.archive.dir");
+		pluginMainDir = runProperties.getPath("plugin.main.dir");
+		logger.info("plugin.main.dir is {}", pluginMainDir);
+		if (pluginMainDir.toFile().list().length != 0) {
+			logger.error("plugin.main.dir is not empty");
+			// force the user to delete the contents manually in case the wrong directory
+			// has been specified because it will be emptied at the end of each test
+			throw new Exception("plugin.main.dir " + pluginMainDir + " must be empty before tests are run");
 		}
 
-		Path cacheDir = runProperties.getPath("cache.dir");
-		preparedCacheDir = cacheDir.resolve(Constants.PREPARED_DIR_NAME);
-		twoLevel = runProperties.has("plugin.archive.class");
-		if (twoLevel) {
-			String storageUnitString = runProperties.getString("storageUnit");
-			if (storageUnitString == null) {
-				throw new Exception("storageUnit not set");
-			}
-			storageUnit = storageUnitString.toUpperCase();
-		}
+		icatSoapClient = ICATGetter.getService(icatUrl.toString());
 
-		errorLog = runProperties.getPath("filesCheck.errorLog");
+		if (skipIcatRepopulate) {
+			logger.warn("Skipping ICAT re-populate");
+		} else {
+			IcatSetup icatSetup = new IcatSetup(icatSoapClient, rootSessionId);
+			icatSetup.cleanIcat();
+			icatSetup.populateIcat();
+			// set the flag so that the ICAT doesn't 
+			// get repopulated for subsequent tests
+			skipIcatRepopulate = true;
+		}
 
 	}
 
-	public void setReliability(double d) throws IOException {
-		Path p = home.resolve("reliability");
-		try (BufferedWriter out = Files.newBufferedWriter(p)) {
-			out.write(d + "\n");
-		}
+
+	public String getRootSessionId() {
+		return rootSessionId;
 	}
 
 	public String getForbiddenSessionId() {
 		return forbiddenSessionId;
 	}
 
-	public String getRootSessionId() {
-		return rootSessionId;
+	public URL getIcatUrl() {
+		return icatUrl;
 	}
 
 	public URL getIdsUrl() {
 		return idsUrl;
 	}
 
-	public Path getStorageArchiveDir() {
-		return storageArchiveDir;
+	public ICAT getIcatSoapClient() {
+		return icatSoapClient;
 	}
 
-	public Path getStorageDir() {
-		return storageDir;
-	}
-
-	public Path getPreparedCacheDir() {
-		return preparedCacheDir;
-	}
-
-	public Path getUpdownDir() {
-		return updownDir;
-	}
-
-	public URL getIcatUrl() {
-		return icatUrl;
-	}
-
-	public String getKey() {
-		return key;
+	public Path getPluginMainDir() {
+		return pluginMainDir;
 	}
 
 }
